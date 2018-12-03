1c5673417f9685e1c225f46a5c175132c70969d2
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-03 15:13:12 +0200

    Refactored supported annotatins validation

--- src/Products/Annotation/Controllers/AnnotationApiController.cs
+++ src/Products/Annotation/Controllers/AnnotationApiController.cs
@@ -443,50 +443,48 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
                     catch (NotSupportedException ex)
                     {
                         notSupportedException = ex;
+                        continue;
                     }
                     catch (Exception ex)
                     {
                         throw new Exception(ex.Message, ex);
                     }
                 }
-                // check if annotations array contains at least one annotation to add
-                if (notSupportedException == null)
+
+                // Add annotation to the document
+                DocumentType type = DocumentTypesConverter.GetDocumentType(documentType);
+                // Save result stream to file.
+
+                string path = GlobalConfiguration.Annotation.OutputDirectory + Path.DirectorySeparatorChar + fileName;
+                if (File.Exists(path))
                 {
-                    // Add annotation to the document
-                    DocumentType type = DocumentTypesConverter.GetDocumentType(documentType);
-                    // Save result stream to file.
-
-                    string path = GlobalConfiguration.Annotation.OutputDirectory + Path.DirectorySeparatorChar + fileName;                   
-                    if (File.Exists(path))
+                    RemoveAnnotations(path);
+                }              
+                // check if annotations array contains at least one annotation to add
+                if (annotations.Count != 0)
+                {
+                    Stream cleanDoc = new FileStream(documentGuid, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    Stream result = AnnotationImageHandler.ExportAnnotationsToDocument(cleanDoc, annotations, type);
+                    cleanDoc.Dispose();
+                    cleanDoc.Close();
+                    // Save result stream to file.                       
+                    using (FileStream fileStream = new FileStream(path, FileMode.Create))
                     {
-                        RemoveAnnotations(path);
-                    }                   
-                    if (annotations.Count != 0)
-                    {
-                        Stream cleanDoc = new FileStream(documentGuid, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-                        Stream result = AnnotationImageHandler.ExportAnnotationsToDocument(cleanDoc, annotations, type);
-                        cleanDoc.Dispose();
-                        cleanDoc.Close();
-                        // Save result stream to file.                       
-                        using (FileStream fileStream = new FileStream(path, FileMode.Create))
-                        {
-                            byte[] buffer = new byte[result.Length];
-                            result.Seek(0, SeekOrigin.Begin);
-                            result.Read(buffer, 0, buffer.Length);
-                            fileStream.Write(buffer, 0, buffer.Length);
-                            fileStream.Close();
-                        }
-                    } 
-                    annotatedDocument = new AnnotatedDocumentEntity()
-                    {
-                        guid = path,
-                    };
-
+                        byte[] buffer = new byte[result.Length];
+                        result.Seek(0, SeekOrigin.Begin);
+                        result.Read(buffer, 0, buffer.Length);
+                        fileStream.Write(buffer, 0, buffer.Length);
+                        fileStream.Close();
+                    }
                 }
-                else
+                else if (notSupportedException != null)
                 {
                     return Request.CreateResponse(HttpStatusCode.OK, new Resources().GenerateException(notSupportedException));
-                }
+                }                
+                annotatedDocument = new AnnotatedDocumentEntity()
+                {
+                    guid = path,
+                };
             }
             catch (Exception ex)
             {
@@ -519,21 +517,21 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
         public void RemoveAnnotations(string path)
         {
             try
-            {                
+            {
                 Stream resultStream = null;
                 string tempFilePath = "";
                 using (Stream inputStream = File.Open(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
                 {
                     resultStream = AnnotationImageHandler.RemoveAnnotationStream(inputStream);
                     resultStream.Position = 0;
-                    tempFilePath = new Resources().GetFreeFileName(GlobalConfiguration.Annotation.OutputDirectory, Path.GetFileName(path));                      
+                    tempFilePath = new Resources().GetFreeFileName(GlobalConfiguration.Annotation.OutputDirectory, Path.GetFileName(path));
                     using (Stream tempFile = File.Create(tempFilePath))
                     {
                         resultStream.Seek(0, SeekOrigin.Begin);
                         resultStream.CopyTo(tempFile);
                     }
                     resultStream.Dispose();
-                    resultStream.Close();                    
+                    resultStream.Close();
                 }
                 File.Delete(path);
                 File.Move(tempFilePath, path);
