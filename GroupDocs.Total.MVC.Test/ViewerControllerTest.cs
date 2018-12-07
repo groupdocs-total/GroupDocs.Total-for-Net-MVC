2194133f3bdc661d40ca8ee40f12c4f08e391bff
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-07 18:00:42 +0200

    Added base for integration tests

---
+++ GroupDocs.Total.MVC.Test/ViewerControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using GroupDocs.Total.MVC.Controllers;
+using NUnit.Framework;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using MvcContrib.TestHelper;
+
+namespace GroupDocs.Total.MVC.Test
+{
+    [TestFixture]
+    public class ViewerControllerTest
+    {  
+        [SetUp]
+        public void TestInitialize()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            RouteTable.Routes.Clear();
+        }
+
+        [Test]
+        public void ViewNameTest() {
+            ViewerController viewer = new ViewerController();
+
+            ViewResult actResult = viewer.Index() as ViewResult;            
+            Assert.That(actResult.ViewName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void ViewStatusTest()
+        {
+            "~/viewer".Route().ShouldMapTo<ViewerController>(x => x.Index());
+        }       
+    }
+}
