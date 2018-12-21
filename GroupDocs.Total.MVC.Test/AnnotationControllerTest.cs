f070ba4f009f8a472f25000840ffe7455a667a1f
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-21 16:02:16 +0200

    Commented out not finished tests

--- GroupDocs.Total.MVC.Test/AnnotationControllerTest.cs
+++ GroupDocs.Total.MVC.Test/AnnotationControllerTest.cs
@@ -54,56 +54,56 @@ namespace GroupDocs.Total.MVC.Test
             "~/annotation".Route().ShouldMapTo<AnnotationController>(x => x.Index());
         }
 
-        [Test]
-        public void FileTreeStatusCodeTest()
-        {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
-            using (var server = new DirectServer(path))
-            {
-                AnnotationPostedDataEntity requestData = new AnnotationPostedDataEntity();
-                requestData.path = "";
+        //[Test]
+        //public void FileTreeStatusCodeTest()
+        //{
+        //    string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+        //    using (var server = new DirectServer(path))
+        //    {
+        //        AnnotationPostedDataEntity requestData = new AnnotationPostedDataEntity();
+        //        requestData.path = "";
 
-                var request = new SerialisableRequest
-                {
-                    Method = "POST",
-                    RequestUri = "/annotation/loadfiletree",
-                    Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestData)),
-                    Headers = new Dictionary<string, string>{
-                        { "Content-Type", "application/json"}
-                    }
-                };
+        //        var request = new SerialisableRequest
+        //        {
+        //            Method = "POST",
+        //            RequestUri = "/annotation/loadfiletree",
+        //            Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestData)),
+        //            Headers = new Dictionary<string, string>{
+        //                { "Content-Type", "application/json"}
+        //            }
+        //        };
 
-                var result = server.DirectCall(request);
-                Assert.That(result.StatusCode, Is.EqualTo(200));
-            }
+        //        var result = server.DirectCall(request);
+        //        Assert.That(result.StatusCode, Is.EqualTo(200));
+        //    }
 
-        }
+        //}
 
 
-        [Test]
-        public void FileTreeDataTest()
-        {
-            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
-            using (var server = new DirectServer(path))
-            {
-                AnnotationPostedDataEntity requestData = new AnnotationPostedDataEntity();
-                requestData.path = "";
+        //[Test]
+        //public void FileTreeDataTest()
+        //{
+        //    string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+        //    using (var server = new DirectServer(path))
+        //    {
+        //        AnnotationPostedDataEntity requestData = new AnnotationPostedDataEntity();
+        //        requestData.path = "";
 
-                var request = new SerialisableRequest
-                {
-                    Method = "POST",
-                    RequestUri = "/annotation/loadfiletree",
-                    Content = null,
-                    Headers = new Dictionary<string, string>{
-                        { "Content-Type", "application/json"}                        
-                    }
-                };
+        //        var request = new SerialisableRequest
+        //        {
+        //            Method = "POST",
+        //            RequestUri = "/annotation/loadfiletree",
+        //            Content = null,
+        //            Headers = new Dictionary<string, string>{
+        //                { "Content-Type", "application/json"}                        
+        //            }
+        //        };
 
-                var result = server.DirectCall(request);
-                var resultString = Encoding.UTF8.GetString(result.Content);
-                dynamic data = JsonConvert.DeserializeObject(resultString);
-                Assert.IsTrue(data.Count > 0);
-            }
-        }
+        //        var result = server.DirectCall(request);
+        //        var resultString = Encoding.UTF8.GetString(result.Content);
+        //        dynamic data = JsonConvert.DeserializeObject(resultString);
+        //        Assert.IsTrue(data.Count > 0);
+        //    }
+        //}
     }
 }
