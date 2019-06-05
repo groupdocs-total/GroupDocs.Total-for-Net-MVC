f1698586d947c92a5eabf4c217bd20a092f9e9a7
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-06-05 16:56:20 +0300

    Added initial version of the editor

--- src/Products/Common/Config/GlobalConfiguration.cs
+++ src/Products/Common/Config/GlobalConfiguration.cs
@@ -3,6 +3,7 @@ using GroupDocs.Total.MVC.Products.Signature.Config;
 using GroupDocs.Total.MVC.Products.Viewer.Config;
 using GroupDocs.Total.MVC.Products.Comparison.Config;
 using GroupDocs.Total.MVC.Products.Conversion.Config;
+using GroupDocs.Total.MVC.Products.Editor.Config;
 
 namespace GroupDocs.Total.MVC.Products.Common.Config
 {
@@ -19,7 +20,8 @@ namespace GroupDocs.Total.MVC.Products.Common.Config
         public AnnotationConfiguration Annotation;
         public ComparisonConfiguration Comparison;
         private readonly ConversionConfiguration Conversion;
-       
+        private readonly EditorConfiguration Editor;
+
         /// <summary>
         /// Get all configurations
         /// </summary>
@@ -33,10 +35,16 @@ namespace GroupDocs.Total.MVC.Products.Common.Config
             Annotation = new AnnotationConfiguration();
             Comparison = new ComparisonConfiguration();
             Conversion = new ConversionConfiguration();
+            Editor = new EditorConfiguration();
         }
 
         public ConversionConfiguration GetConversionConfiguration() {
             return Conversion;
+        }
+
+        public EditorConfiguration GetEditorConfiguration()
+        {
+            return Editor;
         }
     }
 }
---
+++ src/Products/Editor/Controllers/EditorApiController.cs
@@ -0,0 +1,207 @@
+﻿using GroupDocs.Total.MVC.Products.Common.Entity.Web;
+using GroupDocs.Total.MVC.Products.Common.Resources;
+using GroupDocs.Total.MVC.Products.Common.Util.Comparator;
+using GroupDocs.Total.MVC.Products.Editor.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GroupDocs.Total.MVC.Products.Editor.Controllers
+{
+    /// <summary>
+    /// ViewerApiController
+    /// </summary>
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EditorApiController : ApiController
+    {
+
+        private static Common.Config.GlobalConfiguration globalConfiguration;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public EditorApiController()
+        {
+            // Check if filesDirectory is relative or absolute path           
+            globalConfiguration = new Common.Config.GlobalConfiguration();
+        }
+
+        /// <summary>
+        /// Load Viewr configuration
+        /// </summary>       
+        /// <returns>Viewer configuration</returns>
+        [HttpGet]
+        [Route("editor/loadConfig")]
+        public EditorConfiguration LoadConfig()
+        {            
+            return globalConfiguration.GetEditorConfiguration();
+        }
+
+        /// <summary>
+        /// Get all files and directories from storage
+        /// </summary>
+        /// <param name="postedData">Post data</param>
+        /// <returns>List of files and directories</returns>
+        [HttpPost]
+        [Route("editor/loadFileTree")]
+        public HttpResponseMessage loadFileTree(PostedDataEntity postedData)
+        {
+            // get request body       
+            string relDirPath = postedData.path;
+            // get file list from storage path
+            try
+            {
+                // get all the files from a directory
+                if (string.IsNullOrEmpty(relDirPath))
+                {
+                    relDirPath = globalConfiguration.GetEditorConfiguration().GetFilesDirectory();
+                }
+                else
+                {
+                    relDirPath = Path.Combine(globalConfiguration.GetEditorConfiguration().GetFilesDirectory(), relDirPath);
+                }
+
+                List<FileDescriptionEntity> fileList = new List<FileDescriptionEntity>();
+                List<string> allFiles = new List<string>(Directory.GetFiles(relDirPath));
+                allFiles.AddRange(Directory.GetDirectories(relDirPath));
+              
+                allFiles.Sort(new FileNameComparator());
+                allFiles.Sort(new FileTypeComparator());
+
+                foreach (string file in allFiles)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    // check if current file/folder is hidden
+                    if (fileInfo.Attributes.HasFlag(FileAttributes.Hidden) ||
+                        Path.GetFileName(file).Equals(Path.GetFileName(globalConfiguration.GetEditorConfiguration().GetFilesDirectory())) ||
+                        Path.GetFileName(file).Equals(".gitkeep"))
+                    {
+                        // ignore current file and skip to next one
+                        continue;
+                    }
+                    else
+                    {
+                        FileDescriptionEntity fileDescription = new FileDescriptionEntity();                   
+                        fileDescription.guid = Path.GetFullPath(file);
+                        fileDescription.name = Path.GetFileName(file);
+                        // set is directory true/false
+                        fileDescription.isDirectory = fileInfo.Attributes.HasFlag(FileAttributes.Directory);
+                        // set file size
+                        if (!fileDescription.isDirectory)
+                        {
+                            fileDescription.size = fileInfo.Length;
+                        }                        
+                        // add object to array list
+                        fileList.Add(fileDescription);
+                    }
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, fileList);
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new Resources().GenerateException(ex));
+            }
+        }           
+
+        /// <summary>
+        /// Download curerntly viewed document
+        /// </summary>
+        /// <param name="path">Path of the document to download</param>
+        /// <returns>Document stream as attachement</returns>
+        [HttpGet]
+        [Route("editor/downloadDocument")]
+        public HttpResponseMessage DownloadDocument(string path)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (File.Exists(path))
+                {
+                    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                    var fileStream = new FileStream(path, FileMode.Open);
+                    response.Content = new StreamContent(fileStream);
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                    response.Content.Headers.ContentDisposition.FileName = Path.GetFileName(path);
+                    return response;
+                }
+            }
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+
+        /// <summary>
+        /// Upload document
+        /// </summary>
+        /// <param name="postedData">Post data</param>
+        /// <returns>Uploaded document object</returns>
+        [HttpPost]
+        [Route("editor/uploadDocument")]
+        public HttpResponseMessage UploadDocument()
+        {
+            try
+            {
+                string url = HttpContext.Current.Request.Form["url"];
+                // get documents storage path
+                string documentStoragePath = globalConfiguration.GetEditorConfiguration().GetFilesDirectory();
+                bool rewrite = bool.Parse(HttpContext.Current.Request.Form["rewrite"]);
+                string fileSavePath = "";
+                if (string.IsNullOrEmpty(url))
+                {
+                    if (HttpContext.Current.Request.Files.AllKeys != null)
+                    {
+                        // Get the uploaded document from the Files collection
+                        var httpPostedFile = HttpContext.Current.Request.Files["file"];
+                        if (httpPostedFile != null)
+                        {
+                            if (rewrite)
+                            {
+                                // Get the complete file path
+                                fileSavePath = Path.Combine(documentStoragePath, httpPostedFile.FileName);
+                            }
+                            else
+                            {
+                                fileSavePath = Resources.GetFreeFileName(documentStoragePath, httpPostedFile.FileName);
+                            }
+
+                            // Save the uploaded file to "UploadedFiles" folder
+                            httpPostedFile.SaveAs(fileSavePath);
+                        }
+                    }
+                }
+                else
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        // get file name from the URL
+                        Uri uri = new Uri(url);
+                        string fileName = Path.GetFileName(uri.LocalPath);
+                        if (rewrite)
+                        {
+                            // Get the complete file path
+                            fileSavePath = Path.Combine(documentStoragePath, fileName);
+                        }
+                        else
+                        {
+                            fileSavePath = Resources.GetFreeFileName(documentStoragePath, fileName);
+                        }
+                        // Download the Web resource and save it into the current filesystem folder.
+                        client.DownloadFile(url, fileSavePath);
+                    }
+                }
+                UploadedDocumentEntity uploadedDocument = new UploadedDocumentEntity();
+                uploadedDocument.guid = fileSavePath;
+                return Request.CreateResponse(HttpStatusCode.OK, uploadedDocument);
+            }
+            catch (System.Exception ex)
+            {
+                // set exception message
+                return Request.CreateResponse(HttpStatusCode.OK, new Resources().GenerateException(ex));
+            }
+        }       
+    }
+}
