b90ecb5177c3991fef0d353c9566483e6bcdb1c7
Egor <egor.krushinov@gmail.com>
2019-07-12 17:48:35 +0300

    Attempt to import and use viewer component on angular.

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewerModule } from '@groupdocs.examples.angular/viewer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
