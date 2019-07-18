e205bd28c9622d413b61b54de889cf66a9462eaa
Egor <egor.krushinov@gmail.com>
2019-07-18 10:38:28 +0300

    Removing gulp command.

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,13 +4,3 @@ gulp.task('copy', function () {
         .src('./node_modules/@groupdocs.examples.jquery/**')
         .pipe(gulp.dest('./Resources/'))   
 })
-
-var exec = require('child_process').exec;
-
-gulp.task('build', function () {
-    return exec('npm run build-client', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
-})
