38abebd11038e2bfaae2d1dbb7e2f840d8545163
Egor <kegor@users.noreply.github.com>
2019-09-19 12:10:26 +0300

    Temp file now becomes initial. (#71)

--- src/Products/Annotation/Controllers/AnnotationApiController.cs
+++ src/Products/Annotation/Controllers/AnnotationApiController.cs
@@ -586,7 +586,7 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
                 if (annotateDocumentRequest.print)
                 {
                     annotatedDocument.pages = GetAnnotatedPagesForPrint(documentGuid);
-                    File.Delete(documentGuid);
+                    File.Move(documentGuid, annotateDocumentRequest.guid);
                 }
             }
             catch (System.Exception ex)
