f5010a265fbcdf01985a65010222a8859ca87879
Egor <egor.krushinov@aspose.com>
2020-02-27 23:42:16 +0300

    Adding Metadata app.

---
+++ src/Products/Common/Entity/Web/FilePropertyEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GroupDocs.Metadata.Common;
+
+namespace GroupDocs.Total.MVC.Products.Common.Entity.Web
+{
+    /// <summary>
+    /// File property entity
+    /// </summary>
+    public class FilePropertyEntity
+    {
+        public string name { get; set; }
+        public dynamic value { get; set; }
+        public FilePropertyCategory category { get; set; }
+        public bool original { get; set; }
+        public MetadataPropertyType type { get; set; }
+    }
+
+    public enum FilePropertyCategory
+    {
+        BuildIn,
+        Default
+    }
+}
