b541693f0c30d307546b8435b25836fbafee7b6e
Egor <egor.krushinov@aspose.com>
2020-07-27 13:24:18 +0300

    Initial iteration of viewer code refactoring.

--- src/Products/Common/Util/Directory/IDirectoryUtils.cs
+++ src/Products/Common/Util/Directory/IDirectoryUtils.cs
@@ -1,7 +1,7 @@
 ﻿namespace GroupDocs.Total.MVC.Products.Common.Util.Directory
 {
     /// <summary>
-    /// IDirectoryUtils
+    /// IDirectoryUtils.
     /// </summary>
     public interface IDirectoryUtils
     {
