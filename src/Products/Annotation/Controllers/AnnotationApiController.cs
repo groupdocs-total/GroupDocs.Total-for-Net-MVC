605d7500c98954998e511affd7583dfe65d93ea2
Egor <egor.krushinov@aspose.com>
2021-04-29 12:36:02 +0300

    Fix points coordinates calculating for images.

--- src/Products/Annotation/Controllers/AnnotationApiController.cs
+++ src/Products/Annotation/Controllers/AnnotationApiController.cs
@@ -137,18 +137,8 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
             {
                 IDocumentInfo info = annotator.Document.GetDocumentInfo();
                 AnnotationBase[] annotations = annotator.Get().ToArray();
-
                 description.guid = loadDocumentRequest.guid;
-
-                string documentType = string.Empty;
-                if (info.FileType != null)
-                {
-                    documentType = SupportedImageFormats.Contains(info.FileType.Extension) ? "image" : info.FileType.ToString();
-                }
-                else
-                {
-                    documentType = "Portable Document Format";
-                }
+                string documentType = getDocumentType(info);
 
                 description.supportedAnnotations = new SupportedAnnotations().GetSupportedAnnotations(documentType);
 
@@ -170,7 +160,7 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
 
                     if (annotations != null && annotations.Length > 0)
                     {
-                        page.SetAnnotations(AnnotationMapper.MapForPage(annotations, i + 1, info.PagesInfo[i]));
+                        page.SetAnnotations(AnnotationMapper.MapForPage(annotations, i + 1, info.PagesInfo[i], documentType));
                     }
 
                     if (pagesContent.Count > 0)
@@ -216,10 +206,11 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
 
                     IDocumentInfo info = annotator.Document.GetDocumentInfo();
                     AnnotationBase[] annotations = annotator.Get().ToArray();
+                    string documentType = getDocumentType(info);
 
                     if (annotations != null && annotations.Length > 0)
                     {
-                        loadedPage.SetAnnotations(AnnotationMapper.MapForPage(annotations, pageNumber, info.PagesInfo[pageNumber - 1]));
+                        loadedPage.SetAnnotations(AnnotationMapper.MapForPage(annotations, pageNumber, info.PagesInfo[pageNumber - 1], documentType));
                     }
 
                     string encodedImage = Convert.ToBase64String(bytes);
@@ -238,6 +229,21 @@ namespace GroupDocs.Total.MVC.Products.Annotation.Controllers
                 // set exception message
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, new Resources().GenerateException(ex, password));
             }
+        }
+
+        private string getDocumentType(IDocumentInfo info)
+        {
+            string documentType = string.Empty;
+            if (info.FileType != null)
+            {
+                documentType = SupportedImageFormats.Contains(info.FileType.Extension) ? "image" : info.FileType.ToString();
+            }
+            else
+            {
+                documentType = "Portable Document Format";
+            }
+
+            return documentType;
         }
 
         private static List<string> GetAllPagesContent(GroupDocs.Annotation.Annotator annotator, IDocumentInfo pages)
